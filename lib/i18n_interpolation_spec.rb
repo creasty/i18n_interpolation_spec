6810b97b8db02fd734cb4f6f475e2e89562cf9ca
Yuki Iwanaga <yuki@creasty.com>
2015-04-22 14:05:53 +0900

    Write interpolation checker

--- lib/i18n_interpolation_spec.rb
+++ lib/i18n_interpolation_spec.rb
@@ -1,3 +1,4 @@
 require_relative 'i18n_interpolation_spec/version'
+require_relative 'i18n_interpolation_spec/interpolation_checker'
 
 module I18nInterpolationSpec; end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+$LOAD_PATH.unshift File.dirname(__FILE__)
+
+require 'rspec'
+require 'i18n_interpolation_spec'
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+end
